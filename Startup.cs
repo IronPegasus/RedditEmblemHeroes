b5d8bd00180b6d7a30240e1166499a5f122d29c0
IronPegasus <kayleebanana@gmail.com>
2017-02-24 22:54:32 -0600

    Tried disabling debugging?

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace RedditEmblemHeroes
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
@@ -41,7 +41,7 @@ namespace RedditEmblemHeroes
             else
             {
                 app.UseExceptionHandler("/App/Error");
-            }
+            }*/
 
             app.UseStaticFiles();
 
